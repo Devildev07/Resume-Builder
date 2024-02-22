934528288d46b86ac31980cb939e58c0e4902f53
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-23 00:40:50 +0530

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResumeTemplatesComponent } from './templates/resume-templates/resume-templates.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: UserDashboardComponent,
+  },
   {
     path: 'templates',
     title: 'Resume Templates',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HeaderComponent,
-        FooterComponent
-    ]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent],
 })
-export class AppModule { }
+export class AppModule {}
