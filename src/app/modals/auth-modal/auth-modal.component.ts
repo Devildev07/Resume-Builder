3279acaaddc93ca8cc04be5eae3d2202074a7838
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-24 02:41:42 +0530

    ui update

--- src/app/modals/auth-modal/auth-modal.component.ts
+++ src/app/modals/auth-modal/auth-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonServicesService } from 'src/app/services/common-services.service';
 
 @Component({
   selector: 'app-auth-modal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.css',
 })
