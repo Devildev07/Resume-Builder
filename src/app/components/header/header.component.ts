f2ab7c3e2aaef805cf4a6c74ca1e0bd2e59ff46e
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-24 17:34:15 +0530

    dark light mode enable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,11 +10,23 @@ import { CommonServicesService } from 'src/app/services/common-services.service'
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  theme = '';
   constructor(public commonService: CommonServicesService) {}
-  
 
   toggleMobMenu() {
     const mobMenu = document.getElementById('mob-menu');
     mobMenu?.classList.toggle('show-menu');
+  }
+  toggleTheme() {
+    const html = document.documentElement;
+    html.classList.toggle('dark');
+    const currentTheme = html.classList.contains('dark') ? 'dark' : '';
+    localStorage.setItem('theme', currentTheme);
+
+    const savedTheme = localStorage.getItem('theme');
+    // console.log(savedTheme);
+    if (savedTheme) {
+      document.documentElement.classList.add(savedTheme);
+    }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        360: "360deg",
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
