06f27d1ab6d8494e76542154a3c1399ad8b53cdc
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-02-26 01:02:52 +0530

    working on features comp

---
+++ src/app/components/features/features.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  standalone: true,
+  imports: [],
+  templateUrl: './features.component.html',
+  styleUrl: './features.component.css',
+})
+export class FeaturesComponent {
+  features = [
+    {
+      Feature: 'User-Friendly Interface',
+      Description:
+        'Our user-friendly interface makes resume building a breeze. Simply input your information, select a template, and watch as your resume comes to life.',
+    },
+    {
+      Feature: 'Customizable Templates',
+      Description:
+        'Choose from a variety of customizable templates tailored to different industries and career levels. Personalize fonts, colors, and layouts to reflect your unique style.',
+    },
+    {
+      Feature: 'Expert Tips and Suggestions',
+      Description:
+        'Receive expert tips and suggestions throughout the resume-building process. From crafting compelling summaries to highlighting key achievements, our platform guides you every step of the way.',
+    },
+    {
+      Feature: 'Access Anytime, Anywhere',
+      Description:
+        "Access your resume anytime, anywhere, from any device. Whether you're on your desktop, tablet, or smartphone, your resume is always at your fingertips.",
+    },
+  ];
+
+  
+  constructor() {}
+
+  ngOnInit() {
+    this.getFeatures();
+  }
+
+  getFeatures() {
+    console.log(this.features);
+    console.log(typeof this.features);
+    return this.features;
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [FeaturesComponent]
 })
 export class HomeComponent {
 
