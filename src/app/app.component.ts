11f63522c875da63877d0d26b486a8254d21635e
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-03-17 01:51:54 +0530

    dashboard in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonServicesService } from './services/common-services.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'resume-builder';
-  constructor(public commonService: CommonServicesService) { }
+  currentUrl?: string;
+  routerSubscription: any;
+  constructor(
+    public commonService: CommonServicesService,
+    public router: Router
+  ) {}
+
+  ngOnInit() {
+    this.getCurrentUrl();
+  }
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
+  getCurrentUrl() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url
+        // console.log('Current URL:', this.currentUrl);
+      }
+    });
+  }
 }
--- src/app/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/pages/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -13,10 +14,24 @@ export class UserDashboardComponent {
   savedTheme: any = 'light';
 
   dashMenu = [
-    { menu: 'home', pageUrl: '/home', icon: '' },
-    { menu: 'templates', pageUrl: '/templates', icon: '' },
-    { menu: 'profile', pageUrl: '/home', icon: '' },
+    {
+      menu: 'home',
+      pageUrl: '/home',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>',
+    },
+    {
+      menu: 'templates',
+      pageUrl: '/templates',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" /></svg>',
+    },
+    {
+      menu: 'profile',
+      pageUrl: '/home',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>',
+    },
   ];
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   toggleTheme() {
     const html = document.documentElement;
@@ -25,9 +40,13 @@ export class UserDashboardComponent {
     localStorage.setItem('theme', currentTheme);
 
     this.savedTheme = localStorage.getItem('theme');
-    console.log(this.savedTheme);
+    // console.log(this.savedTheme);
     if (this.savedTheme) {
       document.documentElement.classList.add(this.savedTheme);
     }
+  }
+
+  sanitizeSVG(svg: string): any {
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
   }
 }
