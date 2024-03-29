e6d00fa37aa73208ec632a18d36e58eeb0c04ece
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-03-30 03:34:24 +0530

    .

--- src/app/pages/resume-form/resume-form.component.ts
+++ src/app/pages/resume-form/resume-form.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-resume-form',
   standalone: true,
-  imports: [],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
+  imports: [
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './resume-form.component.html',
-  styleUrl: './resume-form.component.css'
+  styleUrl: './resume-form.component.css',
 })
 export class ResumeFormComponent {
+  firstFormGroup: FormGroup | any;
+  secondFormGroup: FormGroup | any;
 
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+   
+  }
 }
