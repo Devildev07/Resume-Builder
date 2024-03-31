0edcbe8a3ae1c1724c7a32a576b6edf32ed3f6d9
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-03-31 21:29:05 +0530

    stepper single form issue fixed

--- src/app/pages/resume-form/resume-form.component.ts
+++ src/app/pages/resume-form/resume-form.component.ts
@@ -8,6 +8,7 @@ import {
   ReactiveFormsModule,
   Validators,
   AbstractControl,
+  ValidationErrors,
 } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,6 +17,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-resume-form',
@@ -42,35 +45,44 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 export class ResumeFormComponent {
   resumetitle: any;
   allResumeData: any = {};
-  firstFormGroup: FormGroup | any;
-  secondFormGroup: FormGroup | any;
+  // firstFormGroup: FormGroup | any;
+  // secondFormGroup: FormGroup | any;
   resumeFormGroup: FormGroup | any;
-  personalDetails: any[] = [];
-  educationalDetails: any[] = [];
+  // personalDetails: any[] = [];
+  // educationalDetails: any[] = [];
+  FormArray: any;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: [
-        '',
-        Validators.required,
-        Validators.email,
-        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
-      ],
-      phone: ['', Validators.required],
-      birthDate: ['', Validators.required],
-      address: ['', Validators.required],
-      city: ['', Validators.required],
-      postalCode: ['', Validators.required],
-      country: ['', Validators.required],
-      website: [''],
-      description: [''],
+    this.resumeFormGroup = this.formBuilder.group({
+      personalDetails: this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        // email: [
+        //   '',
+        //   Validators.required,
+        //   Validators.email,
+        //   Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        // ],
+        email: ['', Validators.required, [this.asyncEmailValidator]],
+        phone: ['', Validators.required],
+        birthDate: ['', Validators.required],
+        address: ['', Validators.required],
+        city: ['', Validators.required],
+        postalCode: ['', Validators.required],
+        country: ['', Validators.required],
+        website: [''],
+        description: [''],
+      }),
+      educationalDetails: this.formBuilder.array([
+        this.createdEduDetailsFormGroup(),
+      ]),
     });
+  }
 
-    this.secondFormGroup = this.formBuilder.group({
+  createdEduDetailsFormGroup(): FormGroup {
+    return this.formBuilder.group({
       institutionName: ['', Validators.required],
       studyField: ['', Validators.required],
       degree: ['', Validators.required],
@@ -80,30 +92,49 @@ export class ResumeFormComponent {
     });
   }
 
-
-  // Usage example
-
-  // submitResumeForm() {
-  //   if (this.resumeFormGroup.valid) {
-  //     this.allResumeData = this.resumeFormGroup.value;
-  //     // this.allResumeData = this.personalDetails.push(this.resumetitle);
-  //     console.log('firstFormGroup data here', this.allResumeData);
-  //   }
-  //   console.log('firstFormGroup not valid enteries');
-  // }
-
-  submitPerDetForm() {
-    if (this.firstFormGroup.valid) {
-      this.personalDetails = this.firstFormGroup.value;
-      // this.allResumeData = this.personalDetails.push(this.resumetitle);
-      console.log('firstFormGroup data here', this.personalDetails);
-    }
+  addEduDetails(): void {
+    const eduDetails = this.resumeFormGroup.get(
+      'educationalDetails'
+    ) as FormArray;
+    eduDetails.push(this.createdEduDetailsFormGroup());
   }
-  submitEduDetForm() {
-    if (this.secondFormGroup.valid) {
-      this.educationalDetails = this.secondFormGroup.value;
-      // this.allResumeData = this.educationalDetails.push(this.resumetitle);
-      console.log('secondFormGroup data here', this.educationalDetails);
+
+  removeEduDetails(i: number): void {
+    const eduDetails = this.resumeFormGroup.get(
+      'educationalDetails'
+    ) as FormArray;
+    eduDetails.removeAt(i);
+  }
+
+  get educationalDetails(): FormArray {
+    return this.resumeFormGroup.get('educationalDetails') as FormArray;
+  }
+
+  submitResumeForm() {
+    if (this.resumeFormGroup.valid) {
+      this.allResumeData = this.resumeFormGroup.value;
+      // this.allResumeData = this.personalDetails.push(this.resumetitle);
+      console.log('firstFormGroup data here', this.allResumeData);
     }
+    console.log('firstFormGroup not valid enteries');
+  }
+
+  asyncEmailValidator(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return new Observable((observer) => {
+      // Simulating asynchronous validation with a timeout
+      setTimeout(() => {
+        if (
+          control.value &&
+          !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(control.value)
+        ) {
+          observer.next({ invalidEmail: true }); // Return validation error
+        } else {
+          observer.next(null); // No error
+        }
+        observer.complete();
+      }, 1000); // Adjust timeout as needed
+    });
   }
 }
