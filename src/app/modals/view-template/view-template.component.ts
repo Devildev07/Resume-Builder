8b199afdc0e5b913c3a9e7d0bd8fc6c7ac40248a
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-04-03 03:24:11 +0530

    in progress

--- src/app/modals/view-template/view-template.component.ts
+++ src/app/modals/view-template/view-template.component.ts
@@ -1,32 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { CommonServicesService } from 'src/app/services/common-services.service';
-import { MatDialogActions, MatDialogClose } from '@angular/material/dialog';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-view-template',
   standalone: true,
-  imports: [MatDialogActions, MatDialogClose],
+  imports: [
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatButtonModule,
+  ],
   templateUrl: './view-template.component.html',
   styleUrl: './view-template.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class ViewTemplateComponent {
-  htmlContent: SafeHtml = '';
+  // htmlContent: SafeHtml = '';
+
+  templateContent: string | any;
+
   constructor(
     public commonService: CommonServicesService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    public dialogRef: MatDialogRef<ViewTemplateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { templateContent: string }
   ) {
-    this.loadHtmlContent();
+    // this.loadHtmlContent();
+
+    this.templateContent = data.templateContent;
   }
-  loadHtmlContent() {
-    this.commonService
-      .getHtmlContent('template-01/temp01.html')
-      .subscribe((content: string) => {
-        let temp = content.replace('Your_name_key', 'dewanshu');
-        this.htmlContent = this.getSafeHtmlContent(temp);
-      });
-  }
-  getSafeHtmlContent(content: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(content);
-  }
+
+  // close() {
+  //   this.dialogRef.close();
+  // }
+  // loadHtmlContent() {
+  //   this.commonService
+  //     .getHtmlContent('template-01/temp01.html')
+  //     .subscribe((content: string) => {
+  //       let temp = content.replace('Your_name_key', 'dewanshu');
+  //       this.htmlContent = this.getSafeHtmlContent(temp);
+  //     });
+  // }
+  // getSafeHtmlContent(content: string): SafeHtml {
+  //   return this.sanitizer.bypassSecurityTrustHtml(content);
+  // }
 }
--- src/app/pages/resume-templates/resume-templates.component.ts
+++ src/app/pages/resume-templates/resume-templates.component.ts
@@ -1,26 +1,72 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { ViewTemplateComponent } from 'src/app/modals/view-template/view-template.component';
+import { CommonServicesService } from 'src/app/services/common-services.service';
 
 @Component({
   selector: 'app-resume-templates',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, RouterModule],
+  encapsulation: ViewEncapsulation.Emulated,
 
   templateUrl: './resume-templates.component.html',
   styleUrl: './resume-templates.component.css',
 })
-export class ResumeTemplatesComponent {
-  constructor(public dialog: MatDialog) {}
-  viewTemplate() {
+export class ResumeTemplatesComponent implements OnInit {
+  templates: any[] = [];
+  constructor(
+    public dialog: MatDialog,
+    public commonService: CommonServicesService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.fetchTemplates();
+  }
+  // viewTemplate() {
+  //   this.dialog.open(ViewTemplateComponent, {
+  //     backdropClass: 'backdrop-blur',
+  //     width: '1000px',
+  //     height: '600px',
+  //     panelClass: 'rounded-md',
+  //     data: '',
+  //   });
+  //   // console.log(formModeClicked);
+  // }
+
+  viewTemplate(template: any) {
+    if (!template || !template.content) {
+      console.error('Invalid template data provided.');
+      return;
+    }
+
     this.dialog.open(ViewTemplateComponent, {
       backdropClass: 'backdrop-blur',
       width: '1000px',
       height: '600px',
       panelClass: 'rounded-md',
-      data: '',
+      data: { templateContent: template.content },
     });
-    // console.log(formModeClicked);
+  }
+
+  fetchTemplates() {
+    const templatesPath = 'assets/templates/'; // Base path for templates
+
+    // Use a loop to iterate through template directories (template-01, template-02, etc.)
+    for (let i = 1; i <= 2; i++) {
+      const templateUrl = `${templatesPath}template-${i
+        .toString()
+        .padStart(2, '0')}/temp${i}.html`;
+      this.http.get(templateUrl, { responseType: 'text' }).subscribe(
+        (templateContent) => {
+          this.templates.push({ content: templateContent });
+        },
+        (error) => {
+          console.error('Error fetching template:', error);
+        }
+      );
+    }
   }
 }
