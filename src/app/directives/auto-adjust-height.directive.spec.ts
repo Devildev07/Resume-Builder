0ec9a341e96889b7552c646ab8113b1633ec59e9
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-05-04 18:21:20 +0530

    autoHeightAdjust for modal content added

---
+++ src/app/directives/auto-adjust-height.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AutoAdjustHeightDirective } from './auto-adjust-height.directive';
+
+describe('AutoAdjustHeightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AutoAdjustHeightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/auto-adjust-height.directive.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appAutoAdjustHeight]',
+  standalone: true
+})
+export class AutoAdjustHeightDirective implements AfterViewInit {
+
+  constructor(public elementRef: ElementRef, public renderer: Renderer2) {
+  }
+
+  ngAfterViewInit() {
+    // setTimeout(() => {
+    this.adjustHeight()
+    // }, 500)
+  }
+
+  adjustHeight() {
+    try {
+      const modal = this.elementRef.nativeElement.closest('.mat-mdc-dialog-container');
+      if (modal) {
+        const modalHeight = modal.clientHeight;
+        const childDivs = this.elementRef.nativeElement.parentElement.children;
+        const topDivHeight = childDivs[0].clientHeight;
+        const bottomDivHeight = childDivs[2].clientHeight;
+        const topBottomHeightSum = topDivHeight + bottomDivHeight;
+        const remainingHeight = modalHeight - topBottomHeightSum;
+        console.log('remainingHeight', remainingHeight)
+        const middleDiv = childDivs[1].querySelector('.mat-mdc-dialog-content');
+
+        if (middleDiv) {
+          middleDiv.style.height = `${remainingHeight - 25}px`;
+        } else {
+          console.warn('Middle div not found');
+        }
+      } else {
+        console.warn('Modal not found');
+      }
+    } catch (error) {
+      console.error('Error adjusting height:', error);
+    }
+  }
+
+}
