d6d21df3812be653285777b897555407a7fce914
Dewanshu Singh <dewanshusingh21@gmail.com>
2024-05-22 16:26:56 +0530

    auth-gaurd update

--- src/app/services/auth/auth-gaurd.guard.ts
+++ src/app/services/auth/auth-gaurd.guard.ts
@@ -3,6 +3,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthServiceService } from './auth-service.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogBoxComponent } from 'src/app/modals/dialog-box/dialog-box.component';
+import { AuthModalComponent } from 'src/app/modals/auth-modal/auth-modal.component';
 
 export const authGaurdGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
@@ -12,19 +13,29 @@ export const authGaurdGuard: CanActivateFn = (route, state) => {
   if (authService.isUsersignin) {
     return true;
   } else {
-    dialog.open(DialogBoxComponent, {
+    // dialog.open(DialogBoxComponent, {
+    //   backdropClass: 'backdrop-blur',
+    //   width: '400px',
+    //   height: 'auto',
+    //   panelClass: 'rounded-lg',
+    //   data: {
+    //     message: 'Please login to continue.',
+    //     title: 'Login alert',
+    //     dialogCss: 'danger-dialog',
+    //     buttonText: 'OK',
+    //     buttonCss: 'danger-dialog-btn',
+    //   },
+    // });
+
+
+    dialog.open(AuthModalComponent, {
       backdropClass: 'backdrop-blur',
-      width: '400px',
-      height: 'auto',
-      panelClass: 'rounded-lg',
-      data: {
-        message: 'Please login to continue.',
-        title: 'Login alert',
-        dialogCss: 'danger-dialog',
-        buttonText: 'OK',
-        buttonCss: 'danger-dialog-btn',
-      },
+      width: '500px',
+      panelClass: 'rounded-md',
+      data: { formMode: 'signup' },
     });
+    // console.log(formModeClicked);
+
     router.navigate(['/home']);
     return false;
   }
